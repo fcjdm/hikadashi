de20c4e7efffcd40b248d8a86bba8f6cc95ef44f
fcjdm <fcjdom@gmail.com>
2023-02-11 18:56:28 +0100

    side nav

--- app/src/main/java/com/android/hikadashi/HostActivity.kt
+++ app/src/main/java/com/android/hikadashi/HostActivity.kt
@@ -1,11 +1,47 @@
 package com.android.hikadashi
 
+import android.content.res.Configuration
+import android.media.Image
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class HostActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
+
+
+
+        drawer = findViewById(R.id.drawer_layout)
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val menuDrawer: ImageView = findViewById(R.id.drawer_toggler)
+        menuDrawer.setOnClickListener(View.OnClickListener {
+            drawer.openDrawer(GravityCompat.START)
+        })
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_item_discover -> Toast.makeText(this, "Discover", Toast.LENGTH_SHORT).show()
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
